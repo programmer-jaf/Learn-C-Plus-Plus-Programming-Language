55024dd99ff6bd8e61f32f8c7a2c936d6efde12e
Programmer-Jaf <programmerjaf@gmail.com>
2025-02-07 19:29:30 +0600

    Demonstrate various data types

---
+++ data-types/src/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Data-types in C++" << endl;
+  /* Primitive data-types
+  int, float, double, char, bool, unsigned int, signed int, long long, unsigned long long
+  int
+  */
+  int a = 124;
+  cout << "Value of a: " << a << endl;
+  /*
+  float
+  */
+  float b = 3.14;
+  cout << "Value of b: " << b << endl;
+  /*
+  double
+  */
+  double c = 2.71828;
+  cout << "Value of c: " << c << endl;
+  /*
+  char
+  */
+  char d = 'A';
+  cout << "Value of d: " << d << endl;
+  /*
+  bool
+  */
+  bool e = true;
+  cout << "Value of e: " << e << endl;
+  /*
+  unsigned int
+  */
+  unsigned int f = 4294967295;
+  cout << "Value of f: " << f << endl;
+  /*
+  signed int
+  */
+  signed int g = -123;
+  cout << "Value of g: " << g << endl;
+  /*
+  long long
+  */
+  long long h = 9223372036854775807;
+  cout << "Value of h: " << h << endl;
+  /*
+  unsigned long long
+  */
+  unsigned long long i = 18446744073709551615;
+  cout << "Value of i: " << i << endl;
+  return 0; 
+}
