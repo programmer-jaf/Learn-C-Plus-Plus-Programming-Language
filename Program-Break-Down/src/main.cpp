ff3ab0a6696f28ea5de50d38399eedbdb064e628
Programmer-Jaf <programmerjaf@gmail.com>
2025-02-07 15:45:13 +0600

    C++ program to demonstrate basic structure and output

---
+++ Program-Break-Down/src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Break down the C Plus Plus Program" << endl;
+  return 0;
+}
+/*
+    1. #include <iostream>
+    2. using namespace std;
+    3. int main() {
+        cout << "Break down the C Plus Plus Program" << endl;
+        return 0;
+    }
+    This C++ program prints the message "Break down the C Plus Plus Program" to the console.
+    The #include directive is used to include the standard input/output library in the program.
+    The using namespace std; directive is used to access the standard namespace.
+    The main() function is the entry point of the program. It contains the program's logic.
+*/
